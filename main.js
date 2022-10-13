51d957fd2a790e0b293f8dd83329992f29d7948a
Orlando <oredwardsjr@gmail.com>
2022-10-12 21:15:41 -0700

    localStorage and unsplash API works

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
 // Variable Declaration
 
 /* DELETE THIS */ KEY = "CAlVNchmiB7bFLzxzpZ97ZXKwHKn1_ijwniyRSHKJxE";
-var posts = localStorage.length + 1;
-
-var image;
-var title;
-var location;
-var description;
-
-const postsDiv = document.querySelector("#posts-content");
-const submitEntryBtn = document.querySelector("#btn-submit");
 
 const accessKey = KEY;
 
+// Starter Function
+displayCachedPosts();
+
 // Event Listeners
-onload = () => { loadLocalStorage() };
+document.querySelector("#btn-submit").addEventListener('click', () => {
+    const post_id = localStorage.getItem('entries');
+    const title = document.querySelector("#destination").value;
+    const dest_location = document.querySelector("#location").value;
+    const description = document.querySelector("#description").value;
 
-submitEntryBtn.addEventListener('click', () => {
-    title = document.querySelector("#destination").value;
     document.querySelector("#destination").value = "";
-
-    dest_location = document.querySelector("#location").value;
     document.querySelector("#location").value = "";
-
-    description = document.querySelector("#description").value;
     document.querySelector("#description").value = "";
 
     // validation
@@ -31,91 +23,93 @@ submitEntryBtn.addEventListener('click', () => {
         alert("Please complete all required fields.")
     }
     else {
-        storePost(title, dest_location, description);
+        add_post(title, dest_location, description, post_id)
     }
 })
 
 // Business Functions
-function loadLocalStorage() {
-    for (var i = 0; i < localStorage.length; i++) {
-        var cached_post = localStorage.getItem(localStorage.key(i));
+function add_post(title, dest_location, description, post_id) {
+    storePost(title, dest_location, description, post_id);
+    displayPost(localStorage.getItem(post_id));
 
-        insertNewPost(cached_post.split(','));
+    localStorage.setItem('entries', parseInt(post_id) + 1);
+}
+
+function storePost(title, dest_location, description, post_id) {
+    localStorage.setItem(post_id, [title, dest_location, description, post_id]);
+}
+
+function displayLocalStorage(cached=true) { 
+    for (var i = 0; i < parseInt(localStorage.entries); i++) {
+        if (localStorage.getItem(i) === null) continue;
+
+        var cached_post = localStorage.getItem(i);
+        
+        displayPost(cached_post, cached); // JSON.parse() can be used here instead
     }
 }
 
-function storePost(title, dest_location, description) {
-    localStorage.setItem(posts, [title, dest_location, description]);
-
-    insertNewPost(localStorage.getItem(posts).split(','));
-
-    posts += 1;
-}
-
-function insertNewPost(post, cached=false) {
-    // .containers
+function displayPost(post_string, cached) { // cached is tracking whether 
+    // declare variables
+    const post = post_string.split(',');
+    const postsDiv = document.querySelector("#posts-content");
     const containersDiv = createDiv();
-    containersDiv.classList.add("containers");
-    containersDiv.id = "container-" + (posts - 1);
-
-    // .image-section
     const imageSectionDiv = createDiv();
-    imageSectionDiv.classList.add("image-section");
-    imageSectionDiv.id = "post-" + posts;
-
-    // .image-info
     const imageInfo = createDiv();
-    imageInfo.classList.add("image-info");
-
     const titleDiv = document.createElement("h1");
-    titleDiv.textContent = post[0];
-
     const locationDiv = document.createElement("h3");
-    locationDiv.textContent = post[1];
-
     const contentPara = document.createElement("p");
-    contentPara.textContent = post[2];
-
-    // .img-btns
     const buttonsDiv = createDiv();
-    buttonsDiv.classList.add("img-btns");
-
     const editBtn = document.createElement("button");
-    editBtn.classList.add("btn-edit");
-    editBtn.textContent = "Edit";
-
     const removeBtn = document.createElement("button");
+
+    // add class
+    containersDiv.classList.add("containers");
+    imageSectionDiv.classList.add("image-section");
+    imageInfo.classList.add("image-info");
+    buttonsDiv.classList.add("img-btns");
+    editBtn.classList.add("btn-edit");
     removeBtn.classList.add("btn-remove");
+            
+    // change textContent
+    titleDiv.textContent = post[0];
+    locationDiv.textContent = post[1];
+    contentPara.textContent = post[2];
+    editBtn.textContent = "Edit";
     removeBtn.textContent = "Remove";
 
-    // Attach Divs
+    // add id
+    containersDiv.id = "container-" + post[3];
+
+    // appendChild
     buttonsDiv.appendChild(editBtn);
     buttonsDiv.appendChild(removeBtn);
-
     imageInfo.appendChild(titleDiv);
     imageInfo.appendChild(locationDiv);
     imageInfo.appendChild(contentPara);
     imageInfo.appendChild(buttonsDiv);
-
     containersDiv.appendChild(imageSectionDiv);
     containersDiv.appendChild(imageInfo);
-    
     postsDiv.appendChild(containersDiv);
 
-    // add event listeners
+    // add eventListener
     editBtn.addEventListener('click', (e) => editContents(e.target))
     removeBtn.addEventListener('click', (e) => removeEntry(e.target))
 
-    loadImg(imageSectionDiv, title, true, cached);
+    // function calls
+    loadImg(imageSectionDiv, post[0], true, cached);
 }
 
-function loadImg(obj, search, first_entry, cached=false) {
-    const url = `https://api.unsplash.com/search/photos?query=${search}&per_page=30&page=1&client_id=${KEY}`;
+function loadImg(obj, search, new_post, cached=false) {
+    // if (cached) {
+    //     obj.src = obj.src;
+    //     console.log(obj);
+    //     return;
+    // }
 
-    if (cached) {
-        obj.src = cached;
-        return
-    }
+    const url = `https://api.unsplash.com/search/photos?query=${search}&per_page=50&page=1&client_id=${KEY}`;
+    const latest_post_id = String(parseInt(localStorage.getItem('entries'))); //
+    const latest_post = localStorage.getItem(latest_post_id);
 
     fetch(url)
         .then(response => {
@@ -127,47 +121,54 @@ function loadImg(obj, search, first_entry, cached=false) {
             for (let i = 0; i < data.results.length; i++) {
                 if (i == randomNum) {
                     let img_result = data.results[i].urls.thumb;
-                    if (first_entry) {
+                    if (new_post) {
                         let image_element = document.createElement('img');
                         image_element.src = img_result;
                         obj.appendChild(image_element);
                     }
-                    else {
+                    else { // if user is editing post and calling for new photo
                         obj.src = img_result;
                     }
-                    var new_entry = localStorage.getItem(posts - 1) + obj.src;
-                    localStorage.setItem(posts - 1, new_entry)
+                    //var new_entry = latest_post + "," + String(obj.src);
+                    //localStorage.setItem(latest_post_id, new_entry);
+                    localStorage.setItem(latest_post_id, latest_post);
                 }
             }
         })
 }
 
 function editContents(obj) {
+    // const declaration
     const obj_container = obj.parentNode.parentNode.parentNode; 
     const image_section = obj_container.children[0]; // .containers.image-section
     const image_info = obj_container.children[1];    // . containers.image-info
-
     const destination = prompt("Enter name of attraction");
     const dest_location = prompt("Enter location of attraction"); 
     const description = prompt("Enter description");
 
-    if (destination) image_info.children[0].textContent = destination;
+    // conditionals
+    if (destination) {
+        image_info.children[0].textContent = destination
+        loadImg(image_section.firstChild, destination, false);
+    }
     if (dest_location) image_info.children[1].textContent = dest_location; 
     if (description) image_info.children[2].textContent = description;
 
-    loadImg(image_section.firstChild, destination);
 }
 
 function removeEntry(obj) {
     parent_node = obj.parentNode.parentNode.parentNode;
     parent_node.remove(); // parent container of post
     
-    console.log(parent_node.id.split('-')[1]);
     localStorage.removeItem(parent_node.id.split('-')[1]);
-    console.log(localStorage);
 }
 
 // Helper Functions
+function displayCachedPosts() {
+    if (localStorage.length === 0) { localStorage.setItem("entries", '0') }
+    else onload = () => { displayLocalStorage() }
+}
+
 function createDiv() {
     return document.createElement("div");
 }
